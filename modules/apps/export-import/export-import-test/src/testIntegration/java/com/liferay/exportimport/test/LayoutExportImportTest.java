57e91cd72ac3247e4e7406975b8b464d8e943151
Jose Luis Navarro <joseluis.navarro@liferay.com>
2026-02-17 07:54:51 +0100

    LPD-78994 rename test

--- modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/LayoutExportImportTest.java
+++ modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/LayoutExportImportTest.java
@@ -962,7 +962,7 @@ public class LayoutExportImportTest extends BaseExportImportTestCase {
 		featureFlags = {@FeatureFlag("LPD-34594"), @FeatureFlag("LPD-35443")}
 	)
 	@Test
-	public void testPromotedPageWithSamePriorityTakesPrecedenceWithPromoteContentFFEnabled()
+	public void testPromotedPageWithSamePriorityTakesPrecedence()
 		throws Exception {
 
 		FeatureFlagTestUtil.invokeFeatureFlagListeners(
