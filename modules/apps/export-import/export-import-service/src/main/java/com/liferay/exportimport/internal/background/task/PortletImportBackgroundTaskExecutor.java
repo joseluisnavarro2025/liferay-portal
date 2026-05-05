06c3b5c5cfdf4d6d320d92aee7c4e60b74a495d0
Jose Luis Navarro <joseluis.navarro@liferay.com>
2026-05-05 10:39:34 +0200

    LPD-83781 use getExportImportReportEntriesCount in import background task executors

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/background/task/PortletImportBackgroundTaskExecutor.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/background/task/PortletImportBackgroundTaskExecutor.java
@@ -9,9 +9,7 @@ import com.liferay.exportimport.internal.background.task.display.PortletExportIm
 import com.liferay.exportimport.kernel.exception.ExportImportIOException;
 import com.liferay.exportimport.kernel.model.ExportImportConfiguration;
 import com.liferay.exportimport.kernel.service.ExportImportLocalService;
-import com.liferay.exportimport.report.model.ExportImportReportEntryTable;
 import com.liferay.exportimport.report.service.ExportImportReportEntryLocalService;
-import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTask;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskExecutor;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskResult;
@@ -111,20 +109,12 @@ public class PortletImportBackgroundTaskExecutor
 			}
 		}
 
-		int count = _exportImportReportEntryLocalService.dslQueryCount(
-			DSLQueryFactoryUtil.count(
-			).from(
-				ExportImportReportEntryTable.INSTANCE
-			).where(
-				ExportImportReportEntryTable.INSTANCE.companyId.eq(
-					exportImportConfiguration.getCompanyId()
-				).and(
-					ExportImportReportEntryTable.INSTANCE.
-						exportImportConfigurationId.eq(
-							exportImportConfiguration.
-								getExportImportConfigurationId())
-				)
-			));
+		int count =
+			_exportImportReportEntryLocalService.
+				getExportImportReportEntriesCount(
+					exportImportConfiguration.getCompanyId(),
+					exportImportConfiguration.
+						getExportImportConfigurationId());
 
 		if (count > 0) {
 			return BackgroundTaskResult.COMPLETED_WITH_ERRORS;
